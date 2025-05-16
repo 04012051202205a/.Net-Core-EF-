49a0979118de194d6382e8727592a6cb72812793
Xuan An <vuxuanan1222005@gmail.com>
2025-05-16 10:51:59 +0700

    Lap 2

--- WebBanHang/Controllers/HomeController.cs
+++ WebBanHang/Controllers/HomeController.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using WebBanHang.Models;
 
 namespace WebBanHang.Controllers
@@ -12,15 +11,19 @@ namespace WebBanHang.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Lấy danh sách sản phẩm kèm theo thông tin Category
+            var productList = _db.Products.Include(p => p.Category).ToList();
+            return View(productList);
         }
 
         public IActionResult Privacy()
